0a163e0b94dacb3ab293db88f42f069ace20c72c
Slav Donchev <srd@Slavs-MacBook-Pro.local>
2019-01-10 11:39:21 +0200

    fix config path var in switch-nginx-traffic.sh

--- vagrant/scripts/switch-nginx-traffic.sh
+++ vagrant/scripts/switch-nginx-traffic.sh
@@ -7,9 +7,9 @@ CONFIG_STORE_PATH="./nginx-config"
 NGINX_CONFIG_PATH="/etc/nginx/sites-available/default"
 
 # replace config and restart nginx
-if [ -f $CONFIG_STORE/$INPUT ]; then
-    sudo cp $CONFIG_STORE/$INPUT $NGINX_CONFIG_PATH
+if [ -f $CONFIG_STORE_PATH/$INPUT ]; then
+    sudo cp $CONFIG_STORE_PATH/$INPUT $NGINX_CONFIG_PATH
     sudo service nginx restart
 else
-    echo "configuration: $CONFIG_STORE/$INPUT does not exist."
+    echo "configuration: $CONFIG_STORE_PATH/$INPUT does not exist."
 fi
